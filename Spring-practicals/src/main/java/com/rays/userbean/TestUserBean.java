5a93f94c44db568e1f3a1fca186074bb7b3fd2e1
aryan190504 <aryanshrivastav2611@gmail.com>
2025-02-06 13:51:46 +0530

    done

---
+++ Spring-practicals/src/main/java/com/rays/userbean/TestUserBean.java
@@ -0,0 +1,19 @@
+package com.rays.userbean;
+
+public class TestUserBean {
+
+	public static void main(String[] args) {
+
+		UserBean bean = new UserBean();
+
+		bean.setLogin("xyx");
+		bean.setPassword("454");
+
+//		UserBean bean = new UserBean("xyz", "456");
+//
+//		System.out.println(bean.getLogin());
+//		System.out.println(bean.getPassword());
+
+	}
+
+}
